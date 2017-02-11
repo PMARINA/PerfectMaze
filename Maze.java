76097a0e4e2685ddc4ac44c392420459f675d998
PMARINA <bluerat33@gmail.com>
2017-02-11 16:46:12 -0500

    Done!

--- Maze.java
+++ Maze.java
@@ -5,9 +5,9 @@ public class Maze{
 	private static boolean[][] east;
 	private static boolean[][] west;
 	private static boolean[][] check;
-	private final static int buffer = 2; //This is how many extra there will be before and after the actual maze's coordinates, to accomodate special cases;
+	private final static int buffer = 1; //This is how many extra there will be before and after the actual maze's coordinates, to accomodate special cases;
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		int n = Integer.parseInt(args[0]); //Process Input
 		n = validateN(n); //Check if n is valid. If not, get a new n 
 		//value. Otherwise, leave n unchanged
@@ -17,6 +17,7 @@ public class Maze{
 		generateMaze(north.length-buffer-1, buffer);//start at bottom 
 		//left corner
 		drawMaze();//output
+		////("The program is now terminating");
 	}
 	/*Takes the north/west/east/south arrays and uses them to draw the maze. 
 	 * Lines will be drawn with length 1, 0.5 above/below/left of/right of 
@@ -35,37 +36,112 @@ public class Maze{
 				if(south[i][j])StdDraw.line(i-0.5,j-0.5,i+0.5,j-0.5);
 				if(east[i][j]) StdDraw.line(i+0.5,j-0.5,i+0.5,j+0.5);
 				if(west[i][j])StdDraw.line(i-0.5,j-0.5,i-0.5,j+0.5);
-				System.out.println("i: " + i + "\nj: " + j);
-				sc.next();
+				////("i: " + i + "\nj: " + j);
+				//sc.next(); //use for debugging purposes -- step-by
+				//step drawing
 			}
 		}
 	}
-	private static void generateMaze(int i, int j){
-		int random = (int)(Math.random()*3);
-		int[] ranToArr = getDirections(random,i,j);
-		int newX = ranToArr[0];
-		int newY = ranToArr[1];
-		if(visNeighbors(i,j)){
-			return;
+	private static void generateMaze(int i, int j)throws Exception{
+		////("call to generateMaze(" + i + ", " + j + ");");
+		// StdDraw.clear();
+		// drawMaze();
+		// StdDraw.textLeft(0,0,"i: " + i+ " & j: " + j);
+		// while( ! StdDraw.hasNextKeyTyped()){
+			// String s= null;
+		// }
+		// StdDraw.nextKeyTyped();
+		if(checkDone() || visNeighbors(i,j)) return;
+		while(! visNeighbors(i,j)){
+			int random = (int)(Math.random()*4.0);
+			int[] ranToArr = getDirections(random,i,j);
+			int newX = ranToArr[0];
+			int newY = ranToArr[1];
+			int counter = 0;
+			while(check[newX][newY]){
+				random = (int )(Math.random() * 4.0);
+				int[] ran = getDirections(random,i,j);
+				newX = ran[0];
+				newY = ran[1];
+			}
+			// StdDraw.clear();
+			// drawMaze();
+			knock(i,j,newX,newY); //knock down the wall between the two cells.
+			check[i][j] = true;
+			////("I: " + i + "\tJ: " + j + "\tNx: " + newX + "\tNy: " + newY);
+			if(!visNeighbors(newX,newY))generateMaze(newX,newY);
+			else check[newX][newY] = true;
+			
 		}
-		while(check[newX][newY]){
-			random = (int )(Math.random() * 3);
-			int[] ran = getDirections(random,i,j);
-			newX = ran[0];
-			newY = ran[1];
+		//("Returning");
+	}
+	private static boolean checkDone(){
+		for(int i = 0; i<check.length;i++){
+			for(int j = 0; j<check[i].length;j++){
+				if(!check[i][j])return false;
+			}
+		}
+		return true;
+	}
+	private static void knock(int i, int j, int nx, int ny)throws Exception{
+		if(i<nx){
+			//We're going right/east
+			east[i][j] = false;
+			west[nx][ny] = false;
+		}
+		else if(i==nx){
+			//We're going north/south
+			//Do a north/south check
+			if(j<ny){
+				//We're going up/north
+				north[i][j] = false;
+				south[nx][ny] = false;
+			}
+			else if(j==ny){
+				while(true){
+					StdDraw.clear(StdDraw.RED);
+					throw new Exception("same point given to knock(" + i + ", " + j+ ", int, int)");
+				}
+			}
+			else{
+				//We're going down/south
+				south[i][j] = false;
+				north[nx][ny] = false;
+			}
+		}
+		else{
+			//We're going left/west
+			west[i][j] = false;
+			east[nx][ny] = false;
 		}
 	}
+
 	/*
 	 * This method checks if all the neighbors of this cell have been
-	 * visited. Note that this will not include diagonals
+	 * visited. Note that this will not include diagonals.
+	 * Test 2/11/17: 1402: exhibiting correct behaviour when absolute
+	 * return true or false
 	 */
-	private static boolean visNeighbors(int i, int j){
-		return check[i+1][j] && check[i][j+1] && check[i-1][j] && check[i][j-1];
+	private static boolean visNeighbors(int i, int j) throws Exception{
+		//return (check[i+1][j] && (check[i][j+1] && (check[i-1][j] && check[i][j-1])));
+		boolean ret = (check[i+1][j] && (check[i][j+1] && (check[i-1][j] && check[i][j-1])));
+		//("I: "+ i + "\tJ: " +j + "\tvisNeighbor returns: " + ret);
+		/*
+		for(int a = 0; a<check.length;a++){
+			for(int b= 0; b<check.length;b++){
+				if(check[a][b])System.out.print("+");
+				else System.out.print("=");
+			}
+			//("");
+		}
+		*/
+		//Thread.sleep(500);
+		return ret;
 	}
 
 	/*
 	 * This method finds the direction that the maze generator should go in
-	 * North = 0, South = 1, East = 2, West = 3+
+	 * North = 0, South = 1, East = 2, West = anyOtherInt
 	 */
 	private static int[] getDirections(int random, int i, int j){
 		if(random ==0){//north
@@ -77,12 +153,21 @@ public class Maze{
 	}
 	private static int validateN(int n){
 		while(n<0){
-			System.out.println("The input is not valid. Please enter a valid input:");
+			////("The input is not valid. Please enter a valid input:");
 			Scanner sc = new Scanner(System.in);
 			n = sc.nextInt();
 		}
 		return n;
 	}
+	/*
+	 * Initialize all the arrays to the correct states:
+	 * N/S/E/W are all true, as walls need to be there
+	 * for all the cells.
+	 * Check is false for all the internal parts, not for
+	 * the buffer parts, as the buffer parts shouldn't be
+	 * used in the generation. This will remove necessity for
+	 * border check in getDirections.
+	 */
 	private static void init(int n){
 		north = new boolean[n+2*buffer][n+2*buffer];
 		for(int i = 0; i<north.length;i++)for(int j = 0; j<north[i].length;j++)north[i][j] = true;
@@ -93,5 +178,7 @@ public class Maze{
 		west  = new boolean[n+2*buffer][n+2*buffer];
 		for(int i = 0; i< west.length;i++)for(int j = 0; j< west[i].length;j++) west[i][j] = true;
 		check = new boolean[n+2*buffer][n+2*buffer];
+		for(int i = 0; i<check.length;i++)for(int j = 0; j<check[i].length;j++) check[i][j] = true;
+		for(int i = buffer; i<check.length-buffer;i++)for(int j = buffer; j<check[i].length-buffer;j++)check[i][j] = false;
 	}
 }
